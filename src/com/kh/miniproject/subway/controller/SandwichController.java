1f8e9ee88841bd3f1b9d82a7c18e79127ebc2cff
als229 <rhksalsdldia@naver.com>
2021-11-10 23:49:58 +0900

    팀프로젝트 주석달기

--- src/com/kh/miniproject/subway/controller/SandwichController.java
+++ src/com/kh/miniproject/subway/controller/SandwichController.java
@@ -4,22 +4,22 @@ import com.kh.miniproject.subway.model.vo.Sandwich;
 
 public class SandwichController {
 	
-	private Sandwich sub = new Sandwich();
+	private Sandwich sub = new Sandwich();   // Sandwich 객체를 만들어서 sub로 변수선언해 줬습니다.
 	
 	public void insertMenu(String name, int price, String bread, String cheese, String vegetable, 
-			String sauce) {
-		
+			String sauce) {  // insertMenu 를 호출하고 매개변수에 값을 입력하면 
+							// Sandwich 타입의 변수 sub에 바로 저장하면서 초기화하여 저장하게 해줬습니다.
 		sub = new Sandwich(name, price, bread, cheese, vegetable, sauce);
 	}
 	
 	public void insertMenu(String name, int price, String bread, String cheese, String vegetable, 
-			String sauce, String cookie, String drink) {
-		
+			String sauce, String cookie, String drink) {  // 세트 메뉴를 받았을때 사용할 insertMenu를 오버로딩으로 생성해 줬습니다.
+ 			
 		sub = new Sandwich(name, price, bread, cheese, vegetable, sauce, cookie, drink);
 	}
 	
 	public Sandwich printMenu() {
-		return sub;
+		return sub;  // printMenu 메서드를 호출하면 초기화된 Sandwich 객체가 저장된 sub 변수를 반환해 줍니다.
 	}
 
 }
--- src/com/kh/miniproject/subway/model/vo/Sandwich.java
+++ src/com/kh/miniproject/subway/model/vo/Sandwich.java
@@ -1,8 +1,8 @@
 package com.kh.miniproject.subway.model.vo;
 
-public class Sandwich {
+public class Sandwich {  // 입력자가 입력한 값을 저장할 Sandwich 클래스 생성
 	
-	private String name;
+	private String name;   // 입력값들을 필드에 private 로 변수 선언
 	private int price;
 	private String bread;
 	private String cheese;
@@ -12,11 +12,11 @@ public class Sandwich {
 	private String drink;
 	
 	
-	public Sandwich() {
+	public Sandwich() {   // 매개변수가 있는 생성자를 사용 할 것이기 때문에 기본생성자는 필수로 만들어줬습니다.
 	}
 	
 	public Sandwich(String name, int price, String bread, String cheese, String vegetable, String sauce) {
-		this.name = name;
+		this.name = name;                // 단품세트를 받았을때 저장할 drink와 cookie 가 없는 생성자를 생성해줬습니다.
 		this.price = price;
 		this.bread = bread;
 		this.cheese = cheese;
@@ -24,7 +24,7 @@ public class Sandwich {
 		this.sauce = sauce;
 	}
 	public Sandwich(String name, int price, String bread, String cheese, String vegetable, 
-			String sauce, String cookie, String drink) {
+			String sauce, String cookie, String drink) { // 세트메뉴를 받았을때 사용할 생성자를 오버로딩으로 cookie와 drink가 있는 생성자를 생성해 줬습니다.
 		this.name = name;
 		this.price = price;
 		this.bread = bread;
@@ -34,6 +34,9 @@ public class Sandwich {
 		this.cookie = cookie;
 		this.drink = drink;
 	}
+	
+	// get, set 메서드들을 만들어 줬습니다.
+	
 	public String getCookie() {
 		return cookie;
 	}
@@ -88,7 +91,7 @@ public class Sandwich {
 	}
 	
 	@Override
-	public String toString() {	
+	public String toString() {	// toString 했을때 고객에게 보여줄 내용틀을 적어줬습니다.
 		return "TMI 드리자면 이 "+name+"은 " +bread +"빵이고, "+cheese +"가 들어갔으며 "
 				+ "야채는 "+ vegetable + " 넣어드렸구요,\n 소스는 " + sauce + "입니다.\n"
 						+ "가격은 " + price +"원입니다 .ㅎ";
